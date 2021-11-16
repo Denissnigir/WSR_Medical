df5f89c462db4094f4e2ed42969a9d8f76af8611
epxch <snigir_00@mail.ru>
2021-11-16 14:24:43 +0300

    Добавьте файлы проекта.

---
+++ WSR_Medical/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WSR_Medical.Model;
+using WSR_Medical.Utils;
+
+namespace WSR_Medical
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImportUser();
+        }
+
+        private void ImportUser()
+        {
+            var fileData = File.ReadAllLines(@"C:\Users\0dmin\Desktop\WorldSkills\8\user.txt");
+
+            foreach(var line in fileData)
+            {
+                var data = line.Split('\t');
+                var date = new DateTime();
+                try
+                {
+                    date = Convert.ToDateTime(data[7]);
+                }
+                catch
+                {
+                    var datestring = data[7].Split('.');
+                    date = Convert.ToDateTime($"{datestring[1]}.{datestring[0]}.{datestring[2]}");
+                }
+                Console.WriteLine(date);
+
+                var tempEmployee = new Employee
+                {
+                    FirstName = data[1],
+                    SecondName = data[2],
+                    MiddleName = data[3],
+                    Login = data[4],
+                    Password = data[5],
+                    Ip = data[6],
+                    Lastenter = date,
+                    RoleId = data[9].ToInt()
+                };
+
+                
+                Context._con.Employee.Add(tempEmployee);
+                Context._con.SaveChanges();
+                foreach(var serviceType in data[8].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentService = Context._con.Service.ToList().FirstOrDefault(p => p.Id == serviceType.ToInt());
+                    if(currentService != null)
+                    {
+                        Context._con.EmployeeService.Add(new EmployeeService { EmployeeId = tempEmployee.Id, ServiceId = currentService.Id });
+                    }
+                }
+                Context._con.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WSR_Medical/Model/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSR_Medical.Model
+{
+    public static class Context
+    {
+        public static WSR_MedicalEntities _con = new WSR_MedicalEntities();
+    }
+}
