82422cb01ebf2b162e4abeeb51aa54bfa0a9ded6
epxch <snigir_00@mail.ru>
2021-11-17 16:22:27 +0300

    Filter

--- WSR_Medical/Pages/SignInHistory.xaml.cs
+++ WSR_Medical/Pages/SignInHistory.xaml.cs
@@ -21,10 +21,56 @@ namespace WSR_Medical.Pages
     /// </summary>
     public partial class SignInHistory : Page
     {
+        List<Employee> employees = new List<Employee>();
+        List<EmployeeEnterArchive> loginList = new List<EmployeeEnterArchive>();
+        List<string> dateList = new List<string>()
+        {
+            "По дате(по возрастанию)",
+            "По дате(по убыванию)"
+        };
         public SignInHistory()
         {
             InitializeComponent();
-            HistoryList.ItemsSource = Context._con.EmployeeEnterArchive.ToList();
+            loginList = Context._con.EmployeeEnterArchive.ToList();
+            employees = Context._con.Employee.ToList();
+            LoginCB.ItemsSource = employees;
+            DateCB.ItemsSource = dateList;
+            DateCB.SelectedIndex = 0;
+            Filter();
+        }
+
+        public void Filter()
+        {
+            if(LoginCB.SelectedIndex > 0)
+            {
+                loginList = loginList.Where(p => p.Login == (LoginCB.SelectedItem as Employee).Login).ToList();
+            }
+
+            HistoryList.ItemsSource = loginList;
+
+            switch (DateCB.SelectedIndex)
+            {
+                case 0:
+                    HistoryList.ItemsSource = loginList.OrderBy(p => p.Date);
+                    break;
+                case 1:
+                    HistoryList.ItemsSource = loginList.OrderByDescending(p => p.Date);
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        private void LoginCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void DateCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
         }
     }
 }
